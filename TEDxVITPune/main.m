dd0ef4d112897c9e6397e27e1a9416ab090ce8ef
Yash Gorana <yash.gorana@hotmail.com>
2016-01-13 19:11:24 +0530

    Initial Commit

---
+++ TEDxVITPune/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TEDxVITPune
+//
+//  Created by Yash Gorana on 13/01/16.
+//  Copyright © 2016 Yash Gorana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
